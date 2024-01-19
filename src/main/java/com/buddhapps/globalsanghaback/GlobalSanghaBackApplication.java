fb68927a77bc959914d6db4616afb3233089b1d3
SergiCots <sergicots93@gmail.com>
2024-01-19 10:13:21 +0100

    initial commit

---
+++ src/main/java/com/buddhapps/globalsanghaback/GlobalSanghaBackApplication.java
@@ -0,0 +1,13 @@
+package com.buddhapps.globalsanghaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSanghaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalSanghaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buddhapps/globalsanghaback/GlobalSanghaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddhapps.globalsanghaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSanghaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
